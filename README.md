8e436c4216fa4d004af1c7a6b780b366843d0664
Devlyn Guarino <ganorith@gmail.com>
2022-03-18 13:01:23 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # DragaliaBPCalc
-A calculator to calculate skill point breakpoints in Dragalia Lost
+A calculator to calculate skill haste breakpoints in Dragalia Lost
+# Explanation of Skill Haste & Breakpoints
+Whenever a character does anything that has some form of skill point increase attatched to it (such as performing a combo 
+by tapping or using the character's force strike), their skills/gauge (depending on the character) fill
+by a certain number depending on the attack. You can use the Skill Haste effect to increase the SP gained 
+by a %. If something would fill a skill by 100 SP, then 15% Skill Haste would make that 115 SP, for example.
+
+However, Skill Haste is only effective if it actually reduces the number of hits it takes to fully fill a skill.
+If a skill has 5000 SP (using sword's c2 > fs combo as an example), the first breakpoint is at 4% skill haste.
+This means that any skill haste from 1-3% would have no effect on making this skill come out faster. This concept
+applies to all breakpoints for this combo (at 4%,7%,11%,20%,25% and so on), any percent of skill haste between those numbers
+would have no effect.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let cList = document.querySelectorAll("#combo input");
+//grab list of character presets from json file and add them to the page
 let comboObj = {}
 fetch("./presets.json").then(response => {
 	return response.json();
@@ -10,7 +10,7 @@ fetch("./presets.json").then(response => {
 	}
 	document.querySelector("div#chars").innerHTML = charList
 })
-
+//toggle for if preset box is open or not
 let presetState = false;
 let togglePreset = function(){
 	if(presetState){
@@ -22,6 +22,9 @@ let togglePreset = function(){
 		presetState = true;
 	}
 }
+//save list of boxes for combo value input from page
+let cList = document.querySelectorAll("#combo input");
+//load character preset into combo boxes
 let charPreset = function(key){
 	cList[0].value = comboObj[key][0]
 	cList[1].value = comboObj[key][1]
@@ -36,6 +39,7 @@ let charPreset = function(key){
 	document.querySelector("#fsc").checked = comboObj[key][10]
 	document.querySelector("#inf").checked = comboObj[key][11]
 }
+//load weapon preset into combo boxes
 let weaponPreset = function(type){	
 	switch(type){
 		case "sword":
@@ -195,6 +199,20 @@ let weaponPreset = function(type){
 	}
 }
 let calculate = function(){
+/* == EXPLANATION OF SKILL HASTE & BREAKPOINTS ==
+Whenever a character does anything that has some form of skill point increase attatched to it (such as performing a combo 
+by tapping or using the character's force strike), their skills/gauge (depending on the character) fill
+by a certain number depending on the attack. You can use the Skill Haste effect to increase the SP gained 
+by a %. If something would fill a skill by 100 SP, then 15% Skill Haste would make that 115 SP, for example.
+
+However, Skill Haste is only effective if it actually reduces the number of hits it takes to fully fill a skill.
+If a skill has 5000 SP (using sword's c2 > fs combo as an example), the first breakpoint is at 4% skill haste.
+This means that any skill haste from 1-3% would have no effect on making this skill come out faster. This concept
+applies to all breakpoints for this combo (at 4%,7%,11%,20%,25% and so on), any percent of skill haste between those numbers
+would have no effect.
+
+all of this is calculated in game using 32 bit floating point numbers too so there are occasional rounding errors thanks mr lost
+*/
 	let spArray = []
 	let	spCost = Number(document.querySelector("#cost").value)
 	let breakpoints = []
@@ -210,14 +228,17 @@ let calculate = function(){
 	let numFS = ""
 	resultsBox.innerHTML = "Please wait..."
 	combo = document.querySelectorAll("#combo input")
+	//check if sp cost was entered
 	if(!spCost){
 		console.log("ERROR: No SP Cost Entered")
 		resultsBox.innerHTML = "<span class='error'>ERROR: No SP Cost Entered.</span>"
 		return;
 	}
+	//check if skill refill has anything entered and lower the spCost if so
 	if(refill){
 		spCost = Math.ceil(spCost * ( 100 - refill) / 100)
 	}
+	//push combo to an array
 	if(combo){
 		for (let i = 0; i < combo.length; i++){
 			if(combo[i].value){
@@ -228,39 +249,49 @@ let calculate = function(){
 	else{
 		spArray = []
 	}
+	//check if any combo was entered
 	if(!spArray[0] && !strikeSP){
 		console.log("ERROR: No SP Combo Entered")
 		resultsBox.innerHTML = "<span class='error'>ERROR: No SP Combo Entered.</span>"
 		return;
 	}
-	
+	//check if the Infinite Combo box is selected and, if so, calculate the skill haste breakpoints
 	if(document.querySelector("#inf").checked){
+		//cut off the last element in the sp combo array
 		spArray.splice(-1,1)
+		//i = skill haste % being checked
 		for (let i = 0; i < 100; i++) {
 			infCombo = 0;
 			currentSP = 0;
 			curCombo = 0;
 			haste = 1 + i/100
+			//check if the c9 value has something entered into it (was the last value in array)
 			let infValue = document.querySelector("#infinite").value * haste
-			
 			if(!infValue){
 				console.log("ERROR: No SP Value entered in infinite slot.")
 				resultsBox.innerHTML = "<span class='error'>ERROR: No SP Value entered in infinite slot.</span>"
 				return;
 			}
+			//increase currentSP by a single combos worth, in the case of characters like Pinon
+			//who have to combo a bit before their infinite combo starts
+			//k = sp increase of hit (from spArray)
 			for (let k = 0; k < spArray.length; k++){
 				spIncrease = Math.ceil(Math.fround(Number(spArray[k])) * Math.fround(haste))
 				currentSP = currentSP + spIncrease 
 			}
+			//increase currentSP by the value of the infinite
 			while(currentSP < spCost){
 				currentSP += infValue
 				curCombo = spArray.length
 				infCombo += 1
+				//send to breakpoint array
 				if (currentSP >= spCost){
+					//if no breakpoint already exists add current breakpoint (should be 0%)...
 					if(!breakpoints[0]){
 						breakpoints.push([infCombo,i])
-						console.log(breakpoints)
 					}
+					//...else check how many hits this combo took compared to the last break point
+					//if its less, push this breakpoint to the array
 					else{
 						let cCheck = breakpoints[breakpoints.length - 1][0]
 						if(infCombo < cCheck){
@@ -270,39 +301,49 @@ let calculate = function(){
 				}
 			}
 		}
-		console.log(spArray)
+		//take results of the breakpoint array and put them into a string
 		if (spArray.length){
 			fullC = "c" + spArray.length + " + "
 		}
 		for (let i = 0; i < breakpoints.length; i++){
 			bpResults += "<div class='bp'><b>" + breakpoints[i][1] + "%</b> " + fullC + breakpoints[i][0] +" hits</div>"
 		}
+		//output the string onto page
 		resultsBox.innerHTML = bpResults
 		return;
 	}
+	//check if the force strike box is equipped
+	//if the player for some reason wants to use striking haste, its applied here
 	if(document.querySelector("#fsc").checked) {
+		//check if fs sp is entered
 		if(!strikeSP){
 			console.log("ERROR: No SP Cost for FS Entered")
 			resultsBox.innerHTML = "<span class='error'>ERROR: No SP Cost for FS slot entered.</span>"
 			return;
 		}
+		//if there is a combo in addition to fs entered, this calculation is used
 		if(spArray.length){
+			//i = skill haste % being checked
 			for (let i = 0; i < 100; i++) {
 				currentSP = 0;
 				curCombo = 0;
 				fsCount = 0
 				haste = 1 + i/100
 				strikeH = haste + strikeHaste
+				//increase currentSP using values in the array
 				while(currentSP < spCost){
+					//k = current position in spArray
 					for (let k = 0; k < spArray.length; k++){
 						spIncrease = Math.ceil(Math.fround(Number(spArray[k])) * Math.fround(haste))
 						currentSP = currentSP + spIncrease
 						curCombo = curCombo + 1;
-						console.log(haste + " " + currentSP + " " + spCost + " " + currentSP >= spCost )
+						//check if no fs is needed for spCost to be met, and if so push to breakpoint array
 						if (currentSP >= spCost){
+							//if no breakpoint already exists add current breakpoint (should be 0%)...
 							if(!breakpoints[0]){
 								breakpoints.push([curCombo,i,fsCount])
 							}
+							//...else compare to last breakpoint
 							else{
 								let cCheck = breakpoints[breakpoints.length - 1][0]
 								if(curCombo < cCheck){
@@ -314,13 +355,17 @@ let calculate = function(){
 							}
 							break;
 						}
+						//if fs is needed in combination of combo
 						else if(k == spArray.length - 1) {
 							currentSP = currentSP + Math.ceil(Math.fround(strikeSP) * Math.fround(strikeH))
 							fsCount += 1
+							//push to breakpoint array
 							if (currentSP >= spCost){
+								//if no breakpoint exists add current breakpoint (should be 0%)...
 								if(!breakpoints[0]){
 									breakpoints.push([curCombo,i,fsCount])
 								}
+								//...else compare to last breakpoint
 								else{
 									let cCheck = breakpoints[breakpoints.length - 1][0]
 									if(curCombo < cCheck){
@@ -330,12 +375,13 @@ let calculate = function(){
 							}
 							break;
 						}
-						console.log(breakpoints)
 					}
 				}
 			}
 		}
+		//if only fs is used
 		else{
+			//i = skill haste % being checked
 			for (let i = 0; i < 100; i++) {
 				currentSP = 0;
 				curCombo = 0;
@@ -345,10 +391,13 @@ let calculate = function(){
 				while(currentSP < spCost){
 					currentSP = currentSP + (strikeSP * strikeH)
 					curCombo += 1;
+					//push to breakpoint array
 					if (currentSP >= spCost){
+								//if no breakpoint exists push current breakpoint
 								if(!breakpoints[0]){
 									breakpoints.push([curCombo,i])
 								}
+								//else compare to previous breakpoint
 								else{
 									let cCheck = breakpoints[breakpoints.length - 1][0]
 									if(curCombo < cCheck){
@@ -360,12 +409,14 @@ let calculate = function(){
 				}
 			}
 		}
+		//if only fs was used, make string out of breakpoints and send output
 		if(onlyFS){
 			for (let i = 0; i < breakpoints.length; i++){
 				numFS += "<div class='bp'><b>" + breakpoints[i][1] + "%</b> FS x" +  breakpoints[i][0] + "</div>"
 			}
 			resultsBox.innerHTML = numFS
 		}
+		//if fs and combo was used, make string out of breakpoints and send output
 		else{
 			for (let i = 0; i < breakpoints.length; i++){
 				let fullC = ""
@@ -388,21 +439,26 @@ let calculate = function(){
 		}
 		return;
 	}
-	
+	//if neither infinite combo or fs are checked, use this calculation
+	//i = skill haste % being checked
 	for (let i = 0; i < 100; i++) {
 		currentSP = 0;
 		curCombo = 0;
 		haste = 1 + i/100
 		
 		while(currentSP < spCost){
+			//k = current point in sp combo
 			for (let k = 0; k < spArray.length; k++){
 				spIncrease = Math.ceil(Math.fround(Number(spArray[k])) * Math.fround(haste))
 				currentSP = currentSP + spIncrease
 				curCombo = curCombo + 1;
+				//push to breakpoint array
 				if (currentSP >= spCost){
+					//if no breakpoint exists add this one (probably still 0%)
 					if(!breakpoints[0]){
 						breakpoints.push([curCombo,i])
 					}
+					//else compare to the previous breakpoint
 					else{
 						let cCheck = breakpoints[breakpoints.length - 1][0]
 						if(curCombo < cCheck){
@@ -414,6 +470,7 @@ let calculate = function(){
 			}
 		}
 	}
+	//take breakpoints and put them in a string and send output
 	for (let i = 0; i < breakpoints.length; i++){
 		let fullC = ""
 		let remC = ""
